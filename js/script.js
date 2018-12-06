ee2dc2eacf87ff29bb92513c5601493706130047
Dainis Putans <bdylan998@gmail.com>
2018-12-06 10:52:19 +0000

    contentslider

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  //set options
+  var speed = 500;              //fade speed
+  var autoswitch = true;      //auto slider option
+  var autoswitch_speed = 4000; //auto slider speed
+
+  //Add initial active class
+  $('.slide').first().addClass('active');
+
+  //Hide all slides
+  $('.slide').hide();
+
+  //Show first slide
+  $('.active').show();
+
+  //next handler
+  $('#next').on('click', nextSlide); 
+
+  $('#prev').on('click', prevSlide); 
+
+  //Auto sliderHandler, set interval function for auto slide
+  if(autoswitch === true){
+      setInterval(nextSlide, autoswitch_speed);
+  }
+
+  // This function will switch to the next slide
+  function nextSlide(){
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':last-child')){
+        $('.slide').first().addClass('active');
+    } else {
+      $('.oldActive').next().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  }
+
+  //Switch to the previous slide
+  function prevSlide(){
+    $('.active').removeClass('active').addClass('oldActive');
+    if($('.oldActive').is(':first-child')){
+        $('.slide').last().addClass('active');
+    } else {
+      $('.oldActive').prev().addClass('active');
+    }
+    $('.oldActive').removeClass('oldActive');
+    $('.slide').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  }
+});
